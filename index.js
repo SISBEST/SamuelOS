c563e950978ae5f2e031924bca56f74d8fea4d7a
Samuel Sharp <samuelsharp4@gmail.com>
2020-08-15 18:02:18 +0000

    feats: create file, auto-detection for highlighting

--- index.js
+++ index.js
@@ -19,9 +19,26 @@ app.get('/', (req, res) => {
 	}
 });
 
+app.get('/api/create', (req, res) => {
+	if(req.query.pass == process.env.PASS) {
+		fs.writeFile('filesystem/' + req.query.filename, '', (err) => {
+			if (err) {
+				res.render('error', {
+					error: err,
+					pass: process.env.PASS
+				});
+			} else {
+				res.redirect('/' + req.query.filename + '/?pass=' + process.env.PASS);
+			}
+		});
+	} else {
+		res.render('noauth');
+	}
+});
+
 app.get('/:file', (req, res) => {
 	if(req.query.pass == process.env.PASS) {
-		fs.readFile('filesystem/' + req.params.file, (err, data) => {
+		fs.readFile('filesystem/' + req.params.file, 'utf8', (err, data) => {
 			if (err) {
 				res.render('file', {
 					content: err,
