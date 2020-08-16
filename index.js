4b408ac9c0501c0e8f961b04adbdadd34d33a9c1
Samuel Sharp <samuelsharp4@gmail.com>
2020-08-16 23:39:43 +0000

    feat: apps

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
 const express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(bodyParser.text());
-const fs = require('fs');
 
 app.get('/', (req, res) => {
+	var config = require('./filesystem/osconfig.json');
 	if(req.query.pass == process.env.PASS) {
-		fs.readFile('filesystem/osconfig.json', 'utf8', (err, data) => {
-			var config = JSON.parse(data);
 			fs.readdir('filesystem', (err, files) => {
-  		res.render('home', {
+			res.render('home', {
 					files: files,
 					pass: process.env.PASS,
-					wallpaper: config.wallpaper
+					wallpaper: config.wallpaper,
+					apps: config.apps,
+					games: config.games
 				});
 			});
-			});
+	} else {
+		res.render('noauth', {
+			wallpaper: config.loginscreen || config.wallpaper
+		});
+	}
+});
+
+app.get('/samuelos', (req, res) => {
+	res.send("Hey robot!")
+});
+
+app.get('/public', (req, res) => {
+	fs.readFile('public.html', 'utf8', (err, data) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.send(data);
+		}
+	});
+});
+
+app.post('/public', (req, res) => {
+	if (req.query.pass == process.env.PASS) {
+		fs.writeFile('public.html', req.body, (err) => {
+			if (err) {
+				res.status(500).send(err);
+			} else {
+				res.send("OK");
+			}
+		});
+	} else {
+		res.status(401).send('AUTH MISSING');
+	}
+});
+
+app.get('/app/:pkg', (req, res) => {
+	if(req.query.pass == process.env.PASS) {
+		res.render('app/' + req.params.pkg, {
+			pass: process.env.PASS,
+			layout: 'editor'
+		});
 	} else {
 		res.render('noauth');
 	}
@@ -59,12 +100,14 @@ app.get('/:file', (req, res) => {
 			if (err) {
 				res.render('file', {
 					content: err,
-					pass: process.env.PASS
+					pass: process.env.PASS,
+					layout: 'editor'
 				});
 			} else {
 				res.render('file', {
 					content: data,
-					pass: process.env.PASS
+					pass: process.env.PASS,
+					layout: 'editor'
 				});
 			}
 		});
