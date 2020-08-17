dea44ee7dfa503a253c2985e19c9bc55e573008c
Samuel Sharp <samuelsharp4@gmail.com>
2020-08-17 00:45:16 +0000

    feat: file uploads

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
+var formidable = require('formidable');
+var v4 = require('uuid').v4;
+var mv = require('mv');
 const fs = require('fs');
 const app = express();
 app.engine('handlebars', exphbs());
@@ -74,6 +77,22 @@ app.get('/api/create', (req, res) => {
 			} else {
 				res.redirect('/?pass=' + process.env.PASS);
 			}
+		});
+	} else {
+		res.render('noauth');
+	}
+});
+
+app.post('/api/upload', (req, res) => {
+	if(req.query.pass == process.env.PASS) {
+		var form = new formidable.IncomingForm();
+		form.parse(req, function (err, fields, files) {
+			var oldpath = files.file.path;
+			var newpath = './filesystem/' + files.file.name;
+			mv(oldpath, newpath, function (err) {
+				if (err) throw err;
+				res.redirect('/?pass=' + process.env.PASS);
+			});
 		});
 	} else {
 		res.render('noauth');
